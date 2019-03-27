4b4d04f09b81298358c1e0af8a435cf5230368e1
alakovic1 <alakovic1@etf.unsa.ba>
2019-03-27 10:49:09 +0100

    aplikacija prikazuje muzicare...

--- app/src/main/java/ba/unsa/etf/rma/VJ_18117/MainActivity.java
+++ app/src/main/java/ba/unsa/etf/rma/VJ_18117/MainActivity.java
@@ -1,7 +1,9 @@
 package ba.unsa.etf.rma.VJ_18117;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AndroidException;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -11,12 +13,12 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private EditText textField;
-    private ListView listView;
+    private Button dugme;
+    private EditText tekst;
+    private ListView lista;
 
     private ArrayList<String> unosi = new ArrayList<String>();
-    private ArrayAdapter<String> adapter;
+    private MyAdapter adapter;
     private ArrayList<Muzicar> muzicari = new ArrayList<>();
 
     @Override
@@ -24,20 +26,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        textField = (EditText) findViewById(R.id.editText);
-        listView = (ListView) findViewById(R.id.listView);
-        adapter = new ArrayAdapter<String>(this,R.layout.element_liste,R.id.Itemname,unosi);
-        listView.setAdapter(adapter);
+        dugme = (Button) findViewById(R.id.button);
+        tekst = (EditText) findViewById(R.id.editText);
+        lista = (ListView) findViewById(R.id.listView);
+        //adapter = new ArrayAdapter<String>(this,R.layout.element_liste,R.id.Itemname,unosi);
+        Resources res = getResources();
+        adapter = new MyAdapter(this, muzicari, res);
+        muzicari.add(new Muzicar("Cardi B", "HIP HOP", "https://en.wikipedia.org/wiki/Cardi_B", "Belcalis Marlenis Almánzar (born October 11, 1992), known professionally as Cardi B, is an American rapper, singer, and songwriter."));
+        muzicari.add(new Muzicar("Ella Fitzgerald", "Jazz", "https://en.wikipedia.org/wiki/Ella_Fitzgerald", "Ella Jane Fitzgerald (April 25, 1917 – June 15, 1996) was an American jazz singer sometimes referred to as the First Lady of Song, Queen of Jazz, and Lady Ella."));
+        muzicari.add(new Muzicar("Freddie Mercury", "Rock'n'Roll", "https://hr.wikipedia.org/wiki/Freddie_Mercury", " Freddie Mercury (Zanzibar, 5. rujna 1946. - London, 24. studenog 1991.), bio je britanski pjevač, tekstopisac i producent, najpoznatiji kao pjevač britanskog rock sastava Queen."));
+        muzicari.add(new Muzicar("Blake Shelton", "Country", "https://en.wikipedia.org/wiki/Blake_Shelton", " Blake Tollison Shelton (born June 18, 1976) is an American country singer, songwriter and television personality."));
+        muzicari.add(new Muzicar("Mozart", "Classical", "https://en.wikipedia.org/wiki/Wolfgang_Amadeus_Mozart", " Wolfgang Amadeus Mozart (27 January 1756 – 5 December 1791), baptised as Johannes Chrysostomus Wolfgangus Theophilus Mozart,[b] was a prolific and influential composer of the classical era."));
+        muzicari.add(new Muzicar("Eminem", "HIP HOP", "https://hr.wikipedia.org/wiki/Eminem", "Marshall Bruce Mathers III (St. Joseph, Missouri, 17. listopada 1972.), poznatiji pod umjetničkim imenom Eminem, stilizirano kao EMINƎM, američki je rap glazbenik, producent i glumac."));
+        muzicari.add(new Muzicar("Mick Jagger", "Rock'n'Roll", "https://en.wikipedia.org/wiki/Mick_Jagger", "Sir Michael Philip Jagger (born 26 July 1943) is an English singer, songwriter, actor and film producer who gained fame as the lead singer and one of the founder members of the Rolling Stones."));
+        lista.setAdapter(adapter);
+        //lista.setAdapter(adapter);
 
-        button.setOnClickListener(new View.OnClickListener(){
+        /*dugme.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                unosi.add(0,textField.getText().toString());
+                unosi.add(0,tekst.getText().toString());
                 adapter.notifyDataSetChanged();
-                textField.setText("");
+                tekst.setText("");
             }
-        });
+        });*/
 
     }
 
